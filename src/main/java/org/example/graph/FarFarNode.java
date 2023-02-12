f4bbb5c0941f1e3d7c95d9e623b282960e33d113
parkjs <plannist19@gmail.com>
2023-02-12 23:39:40 +0900

    d

--- src/main/java/org/example/graph/FarFarNode.java
+++ src/main/java/org/example/graph/FarFarNode.java
@@ -2,9 +2,7 @@ package org.example.graph;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 public class FarFarNode {
@@ -38,26 +36,43 @@ public class FarFarNode {
         FarFarNode farFarNode = new FarFarNode();
         for(int i=1; i<list.size(); i++){
             if(!flag[i])
-                farFarNode.dfs(i, 0);
+//                farFarNode.dfs(i, 0);
+                farFarNode.bfs(i);
         }
 
         log.info("list: {}", list);
 
         log.info("max: {}", max);
-
-
-
     }
 
+    public void bfs(int i){
+        flag[i] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+
+        while (!q.isEmpty()){
+            int k = q.poll();
+            for(int n =0; n<list.get(k).size(); n++){
+                if(!flag[list.get(k).get(n)]){
+                    flag[list.get(k).get(n)] = true;
+                    q.add(list.get(k).get(n));
+                }
+            }
+            max++;
+        }
+    }
+
+    //dfs로 풀수 없는 이유는  1번과의 거리 체크 불가능함.
+    //ex) 1 > 3 > 6 > 3 > 4 > 2 > 5  에서 6번노드까지 최단거리측정했고 2번 움직였지만 다시 1번 노드를 방문하지않고 3번에서 분기 되어 이동하므로
     public void dfs(int i, int cnt){
-        log.info("방문노드: {}", i);
+        log.info("방문노드: {}, cnt: {}", i, cnt);
         flag[i] = true;
         if(cnt > max){
             max = cnt;
         }
         for(int k : list.get(i)){
             if(!flag[k]){
-                dfs(k, cnt+1);
+                dfs(k, cnt + 1);
             }
         }
     }
