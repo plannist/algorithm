3ea8b00c0d740a08f0b96a3d7dd1e279adbc6fca
parkjs <plannist19@gmail.com>
2023-02-12 22:49:50 +0900

    d

--- src/main/java/org/example/graph/FarFarNode.java
+++ src/main/java/org/example/graph/FarFarNode.java
@@ -3,46 +3,62 @@ package org.example.graph;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 public class FarFarNode {
 
     static int [][] graph;
-    static boolean [][] visitied;
+    static boolean [][] visited;
 
-    public static void main(String [] args){
-        int [][] ins = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+    static List<List<Integer>> list = new ArrayList<>();;
+    static boolean [] flag;
+
+    static int max;
+
+    public static void main(String [] args) {
+        int[][] ins = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
 
 
-        graph    = new int[ins.length][ins.length];
-        visitied = new boolean[ins.length][ins.length];
+        graph = new int[ins.length][ins.length];
+        visited = new boolean[ins.length][ins.length];
 
-        for(int i=0; i<ins.length; i++){
-            for(int j=0; j<ins[i].length; j++){
-                graph[ins[i][0]][ins[i][1]] = 1;
-                graph[ins[i][1]][ins[i][0]] = 1;
-            }
+
+        flag = new boolean[ins.length + 1];
+        for (int i = 0; i < ins.length; i++) {
+            list.add(i, new ArrayList<>());
         }
 
-        for(int[] ls : graph){
-            log.info("ls: {}", ls);
+        for (int i = 0; i < ins.length; i++) {
+            list.get(ins[i][0]).add(ins[i][1]);
+            list.get(ins[i][1]).add(ins[i][0]);
         }
 
         FarFarNode farFarNode = new FarFarNode();
-        farFarNode.dfs(1, 0);
+        for(int i=1; i<list.size(); i++){
+            if(!flag[i])
+                farFarNode.dfs(i, 0);
+        }
+
+        log.info("list: {}", list);
+
+        log.info("max: {}", max);
+
 
 
     }
 
-    public void dfs(int n, int sum){
-        for(int k =1; k< graph[n].length; k++){
-            visitied[n][k] = true;
-            if(graph[n][k] > 0){
-                dfs(k, sum+1);
+    public void dfs(int i, int cnt){
+        log.info("방문노드: {}", i);
+        flag[i] = true;
+        if(cnt > max){
+            max = cnt;
+        }
+        for(int k : list.get(i)){
+            if(!flag[k]){
+                dfs(k, cnt+1);
             }
-
-
         }
     }
 }
